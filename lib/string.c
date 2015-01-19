5fc64d729efa74022847639bad3eab4637ce4e31
David Schor <davidschor@zigwap.com>
2015-01-19 18:29:47 -0500

    ported $mid()

--- lib/string.c
+++ lib/string.c
@@ -60,7 +60,7 @@ MSL_IDENTIFIER_L(str)
 {
     size_t len;
     long repeat;
-    char *r, *tmp, *str;
+    char *ret, *tmp, *str;
     
     if (MSL_ARG_COUNT > 2)
         MSL_COMMAND_THROW(muErr_TooManyParams);
@@ -74,23 +74,23 @@ MSL_IDENTIFIER_L(str)
     str = MSL_GET_ARG(0);
     len = strlen(str);
     
-    if (!(r = malloc(len * repeat + 1)))
+    if (!(ret = malloc(len * repeat + 1)))
         MSL_COMMAND_THROW(muErr_OOM);
         
-    for (tmp = r; repeat--;)
+    for (tmp = ret; repeat--;)
     {
         memcpy(tmp, str, len); 
         tmp += len;
     }
     *tmp = '\0';
     
-    MSL_RETURN(r);
+    MSL_RETURN(ret);
 }
 /* {{
  *      ID:
  *           $left()
  *  Syntax:
- *           $left(<string>,<N>)
+ *           $left(<string>, <N>)
  *    Desc:
  *           Returns a substring from the left side of string
  *           based on <N>. Where:
@@ -108,7 +108,7 @@ MSL_IDENTIFIER_L(str)
 MSL_IDENTIFIER_L(left)
 {
     size_t len;
-    char *r, *tmp, *str;
+    char *ret, *tmp, *str;
     long n;
     
     if (MSL_ARG_COUNT > 2)
@@ -137,9 +137,167 @@ MSL_IDENTIFIER_L(left)
             len = n;
     }
     
-    if (!(r = malloc(len + 1)))
+    if (!(ret = malloc(len + 1)))
         MSL_COMMAND_THROW(muErr_OOM);
     
-    memcpy(r, str, len);
-    MSL_RETURN(r);
+    memcpy(ret, str, len);
+    ret[len] = '\0';
+    
+    MSL_RETURN(ret);
+}
+/* {{
+ *      ID:
+ *           $right()
+ *  Syntax:
+ *           $right(<string>, <N>)
+ *    Desc:
+ *           Returns a substring from the right side of string
+ *           based on <N>. Where:
+ * 
+ *           If <N> >= 0: return <N> characters from the right.
+ *           If <N> <  0: return everything from the right minus
+ *                         <N> characters.
+ *  Throws:
+ *           - muErr_TooManyParams
+ *           - muErr_TooFewParams
+ *           - muErr_InvalidParams
+ *           - muErr_OOM
+ * }}
+ */
+MSL_IDENTIFIER_L(right)
+{
+    size_t len, start;
+    char *ret, *tmp, *str;
+    long n;
+    
+    if (MSL_ARG_COUNT > 2)
+        MSL_COMMAND_THROW(muErr_TooManyParams);
+    if (MSL_ARG_COUNT < 2)
+        MSL_COMMAND_THROW(muErr_TooFewParams);
+    
+    n = strtol(MSL_GET_ARG(1), &tmp, 10);
+    if (*tmp != '\0')
+        MSL_COMMAND_THROW(muErr_InvalidParams);
+    
+    str = MSL_GET_ARG(0);
+    len = strlen(str);
+    
+    if (n >= 0)
+    { /* start on Nth character from left */
+        if (n > len)
+            start = 0;
+        else
+            start = len - n;
+    }
+    else
+    { /* subtract N characters from the start */
+        n = -n;
+        if (n > len)
+            start = len;
+        else
+            start = n;
+    }
+    
+    len = len - start;
+    if (!(ret = malloc(len + 1)))
+        MSL_COMMAND_THROW(muErr_OOM);
+    
+    memcpy(ret, str + start, len);
+    ret[len] = '\0';
+    
+    MSL_RETURN(ret);
+}
+/* {{
+ *      ID:
+ *           $mid()
+ *  Syntax:
+ *           $mid(<string>, <S>[, <L>])
+ *    Desc:
+ *           Returns a substring starting at position <S> for
+ *           a length of <L>, where:
+ * 
+ *           If <S> >= 0: the start is from the <S>th character.
+ *           If <S> <  0: the start if from the -<S>th character from the end.
+ * 
+ *           If <L> > 0: return <L> characters.
+ *           If <L> < 0: return <L> characters from the end of the substring.
+ *           If <L> = 0: return length (WHY WTF?)
+ *  Throws:
+ *           - muErr_TooManyParams
+ *           - muErr_TooFewParams
+ *           - muErr_InvalidParams
+ *           - muErr_OOM
+ * }}
+ */
+MSL_IDENTIFIER_L(mid)
+{
+    size_t len;
+    char *ret, *tmp, *str;
+    long start;
+    
+    if (MSL_ARG_COUNT > 3)
+        MSL_COMMAND_THROW(muErr_TooManyParams);
+    if (MSL_ARG_COUNT < 1)
+        MSL_COMMAND_THROW(muErr_TooFewParams);
+    
+    start = strtol(MSL_GET_ARG(1), &tmp, 10);
+    if (*tmp != '\0')
+        MSL_COMMAND_THROW(muErr_InvalidParams);   
+    
+    str = MSL_GET_ARG(0);
+    len = strlen(str);
+    
+    /* $mid( , 1) AND $mid( , 0) are the same, so we just shift
+     * everything and zero-based it. */
+    if (start > 0) --start;
+    if (start < 0)
+    { /* the start is number of character from the end */
+        start = -start;
+        if (start <= len)
+            start = len - start;
+        else
+            start = len;
+    }
+    if (start > len)
+        start = len;
+    
+    /* narrow down the length to the new length */
+    len -= start;
+    
+    /* if the length is provided, apply it on top of the previous
+     * substring */
+    if (MSL_ARG_COUNT == 3)
+    {
+        int newlen;
+        
+        newlen = strtol(MSL_GET_ARG(2), &tmp, 10);
+        if (*tmp != '\0')
+            MSL_COMMAND_THROW(muErr_InvalidParams);
+        
+        /* this is serious a wtf? why dos mIRC even do this? */
+        if (newlen == 0) /* on N=0, it actually returns the LENGTH, just wtf? */
+            MSL_RETURN_PINT(len);
+        
+        if (newlen > 0)
+        { /* reduce the size down to the newlen */
+            if (newlen < len)
+                len = newlen;
+        }
+        else
+        {
+            newlen = -newlen;
+            if (newlen < len)
+                len -= newlen;
+            else /* -N > len of substring */
+                len = start;
+        }
+    }
+    
+    if (!(ret = malloc(len + 1)))
+        MSL_COMMAND_THROW(muErr_OOM);
+    
+    memcpy(ret, str + start, len);
+    ret[len] = '\0';
+    
+    MSL_RETURN(ret);
 }
